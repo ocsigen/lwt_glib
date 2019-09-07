8295bffe59e12c5060cabb5e37e0ba792d9c2691
Anton Bachin <antonbachin@yahoo.com>
2019-09-07 08:53:53 +0300

    Travis: don't install system compiler

--- src/travis.sh
+++ src/travis.sh
@@ -5,23 +5,39 @@ set -x
 # Install system packages.
 case $TRAVIS_OS_NAME in
     linux)
-        wget https://github.com/ocaml/opam/releases/download/2.0.5/opam-2.0.5-x86_64-linux
-        sudo mv opam-2.0.5-x86_64-linux /usr/local/bin/opam
-        sudo chmod a+x /usr/local/bin/opam
         sudo apt-get update -qq
         sudo apt-get install -qq libev-dev
         ;;
     osx)
         brew update > /dev/null
-        brew install libev gtk+ ocaml opam
+        brew install libev gtk+
         ;;
 esac
 
 
 
-# Initialize opam.
+# Install opam.
+VERSION=2.0.5
+
+case "$TRAVIS_OS_NAME" in
+    linux) OS=linux;;
+      osx) OS=macos;;
+        *) echo Unsupported system $TRAVIS_OS_NAME; exit 1;;
+esac
+
+FILENAME=opam-$VERSION-x86_64-$OS
+
+wget https://github.com/ocaml/opam/releases/download/$VERSION/$FILENAME
+sudo mv $FILENAME /usr/local/bin/opam
+sudo chmod a+x /usr/local/bin/opam
+
 opam init -y --bare --disable-sandboxing --disable-shell-hook
-opam switch create . $COMPILER $REPOSITORIES --no-install
+if [ ! -d _opam/bin ]
+then
+    rm -rf _opam
+    opam switch create . $COMPILER $REPOSITORIES --no-install
+fi
+
 eval `opam config env`
 opam --version
 ocaml -version
